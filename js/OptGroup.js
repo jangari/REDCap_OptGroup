49076874c0048b6a0f0136df6badbca2cfcf141d
Günther Rezniczek <g.rezniczek@gmail.com>
2025-03-01 16:39:57 +0100

    Setup for handling radios

--- js/OptGroup.js
+++ js/OptGroup.js
@@ -15,13 +15,14 @@ function initialize(fields, jsmo) {
     // Store config
     config.fields = fields;
     config.JSMO = jsmo;
+    config.mlmActive = false;
     // Listen for DOMContentLoaded
     document.addEventListener("DOMContentLoaded", render);
 }
 
 function render() {
-    var mlmActive = config.JSMO.isMlmActive();
-    if (mlmActive) {
+    config.mlmActive = config.JSMO.isMlmActive();
+    if (config.mlmActive) {
         config.JSMO.afterRender(function() {
             // Need to update optgroup label
             document.querySelectorAll("[mlm-optgroup-label]").forEach(function(option) {
@@ -32,54 +33,67 @@ function render() {
     }
     Object.keys(config.fields).forEach(function(fieldName) {
         var select = document.querySelector("select[name='" + fieldName + "']");
-        if (!select) return; // Skip if no matching select found
-
-        var groupValues = config.fields[fieldName];
-        var options = Array.from(select.options);
-        var fragment = document.createDocumentFragment();
-        var currentOptGroup = null;
-        var foundValues = new Set(options.map(option => option.value));
-        var selectedValue = select.value;
-
-        groupValues.forEach(function(groupValue) {
-            if (!foundValues.has(groupValue)) {
-                console.warn("Optgroup value '" + groupValue + "' for field '" + fieldName + "' is missing from dropdown choices.");
-            }
-        });
-        if (select.classList.contains('rc-autocomplete')) {
-            // When autocomplete is enabled, the action tag will break down. To preserve the intent, we simply remove all options marked as group labels
-            options.forEach(function(option) {
-                if (groupValues.includes(option.value)) {
-                    $(option).remove();
-                }
-            });
-            return; // Done
+        if (select) {
+            render_dropdown(select, fieldName);
+            return;
         }
-        options.forEach(function(option) {
-            if (groupValues.includes(option.value)) {
-                currentOptGroup = document.createElement("optgroup");
-                currentOptGroup.setAttribute("choice", option.value);
-                currentOptGroup.label = option.textContent.trim();
-                if (mlmActive) {
-                    // Hide the original option but add it so that MLM can translate it, and add a marker
-                    option.style.display = "none";
-                    option.setAttribute("mlm-optgroup-label", "1");
-                    option.setAttribute("disabled", "disabled");
-                    currentOptGroup.appendChild(option);
-                }
-                fragment.appendChild(currentOptGroup);
-                return;
-            }
-            (currentOptGroup || fragment).appendChild(option);
-        });
-
-        select.innerHTML = "";
-        select.appendChild(fragment);
-        // Restore selected value
-        select.value = selectedValue;
+        var radioLabel = document.querySelector("label[data-mlm-field='" + fieldName + "'][data-mlm-type='enum']");
+        if (radioLabel) {
+            render_radio(fieldName);
+            return;
+        }
     });
 }
 
+function render_dropdown(select, fieldName) {
+    var groupValues = config.fields[fieldName];
+    var options = Array.from(select.options);
+    var fragment = document.createDocumentFragment();
+    var currentOptGroup = null;
+    var foundValues = new Set(options.map(option => option.value));
+    var selectedValue = select.value;
+
+    groupValues.forEach(function(groupValue) {
+        if (!foundValues.has(groupValue)) {
+            console.warn("Optgroup value '" + groupValue + "' for field '" + fieldName + "' is missing from dropdown choices.");
+        }
+    });
+    if (select.classList.contains('rc-autocomplete')) {
+        // When autocomplete is enabled, the action tag will break down. To preserve the intent, we simply remove all options marked as group labels
+        options.forEach(function(option) {
+            if (groupValues.includes(option.value)) {
+                $(option).remove();
+            }
+        });
+        return; // Done
+    }
+    options.forEach(function(option) {
+        if (groupValues.includes(option.value)) {
+            currentOptGroup = document.createElement("optgroup");
+            currentOptGroup.setAttribute("choice", option.value);
+            currentOptGroup.label = option.textContent.trim();
+            if (config.mlmActive) {
+                // Hide the original option but add it so that MLM can translate it, and add a marker
+                option.style.display = "none";
+                option.setAttribute("mlm-optgroup-label", "1");
+                option.setAttribute("disabled", "disabled");
+                currentOptGroup.appendChild(option);
+            }
+            fragment.appendChild(currentOptGroup);
+            return;
+        }
+        (currentOptGroup || fragment).appendChild(option);
+    });
+
+    select.innerHTML = "";
+    select.appendChild(fragment);
+    // Restore selected value
+    select.value = selectedValue;
+}
+
+function render_radio(fieldName) {
+    console.log('Radio: ' + fieldName)
+}
 
 
 })();
